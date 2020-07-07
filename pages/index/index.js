b37c6e2cc832ed8a00016bda3bf35b9c190a0684
zhoufan <zhoufan@zhoufandeMacBook-Pro.local>
2020-07-07 12:46:51 +0800

    更正了添加之后表单数据不重置，以及表单内容为空时进行弹窗提示

--- pages/index/index.js
+++ pages/index/index.js
@@ -45,7 +45,7 @@ Page({
     // 弹窗数据
     date: '',
     content: '',
-    leave: '正常'
+    leave: '正常',
   },
   // 导航栏点击事件
   onNavTap(event){
@@ -62,7 +62,6 @@ Page({
   },
   // 弹窗单选变化
   radioChange(event){
-    console.log(event.detail.value);
     this.setData({
       leave: event.detail.value
     });
@@ -70,29 +69,13 @@ Page({
   // input输入发生变化
   onInputChange(event){
     this.setData({
-      content: event.detail.value
+      content: event.detail.value,
     });
   },
   // 添加新事项
   onTapDialogButton(event){
     let data = {};
     if(event.detail.index === 0 && this.data.content !== ''){
-      // this.selectComponent('#form').validate((valid, errors) => {
-      //   console.log('valid', valid, errors)
-      //   if (!valid) {
-      //       const firstError = Object.keys(errors)
-      //       if (firstError.length) {
-      //           this.setData({
-      //               error: errors[firstError[0]].message
-      //           })
-
-      //       }
-      //   } else {
-      //       wx.showToast({
-      //           title: '校验通过'
-      //       })
-      //   }
-      // });
       let time = new Date();
       let newData = this.data.contentData;
       data = {
@@ -118,6 +101,11 @@ Page({
         // leave: '正常',
         content: ''
       });
+    }else if(this.data.content === ''){
+      wx.showToast({
+        title: '内容为必填项',
+        icon: 'none'
+      })
     }else{
       this.setData({
         dialogIsShow: false,
@@ -133,33 +121,5 @@ Page({
     this.setData({
       [key]: !this.data.contentData[index].flag
     });
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
   },
 })
